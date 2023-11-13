fc745171bb66b29a74a53da124af8c3335ee8908
Marko <50851469+makilezx@users.noreply.github.com>
2023-11-13 01:11:37 +0100

    Update Latent class analysis.r
    minor fix

--- Latent class analysis.r
+++ Latent class analysis.r
@@ -6,19 +6,20 @@ library(haven)
 library(dplyr)          
 library(mclust)
 
+dataset <- read_sav("...")
+
+
 colnames(dataset)
 LHT.sum <- subset(dataset, select = c(316:322))
+
 
 colnames(LHT.sum)
 describe(LHT.sum)
 
 
 LHT.sum.mclust. <- Mclust(LHT.sum, g = 1:9)
-summary(LHT.sum.mclust.)
-LHT.sum.mclust.$bic
-LHT.sum.mclust.$parameters
-LHT.sum.mclust.$classification
 
-matrica.sum.klase.LHT <- cbind(LHT.std, LHT.sum.mclust.$classification)
-colnames(matrica.sum.klase.LHT)
-describe.by(matrica.sum.klase.LHT, group = LHT.sum.mclust.$classification)
+print(LHT.sum.mclust_results$BIC)
+print(LHT.sum.mclust_results$parameters)
+print(LHT.sum.mclust_results$classification)
+
